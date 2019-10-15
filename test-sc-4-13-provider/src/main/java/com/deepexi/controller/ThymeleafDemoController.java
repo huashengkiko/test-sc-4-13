64051aa17c691ab3fb155f74102e1ebdcd865181
test <test@qq.com>
2019-10-15 17:45:36 +0800

    source code push

---
+++ test-sc-4-13-provider/src/main/java/com/deepexi/controller/ThymeleafDemoController.java
@@ -0,0 +1,14 @@
+package com.deepexi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller("/demo/thymeleaf")
+public class ThymeleafDemoController {
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("greeting", "hello thymeleaf!");
+        return "demo_page";
+    }
+}
